f743eb5e191df95e1386365595078ed7378714a1
Matheus7p <matheus.costa.7p@gmail.com>
2024-10-02 18:18:42 -0300

    feat: delete product

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify"
 import { CreateProductController } from "./controllers/CreateProductController"
 import { ListProductController } from "./controllers/ListProductController";
+import { DeleteProductController } from "./controllers/DeleteProductController";
 
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
@@ -10,5 +11,9 @@ export async function routes(fastify: FastifyInstance, options: FastifyPluginOpt
 
     fastify.get("/products", async (request: FastifyRequest, reply: FastifyReply) => {
       return new ListProductController().handle(request, reply);
+    })
+
+    fastify.delete("/product", async (request: FastifyRequest, reply: FastifyReply) => {
+      return new DeleteProductController().handle(request, reply);
     })
 }
